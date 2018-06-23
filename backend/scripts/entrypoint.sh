ad8fd2111c8b86871ba9f340933e883db7cf2851
Sparhawk4 <b.wozniak4@gmail.com>
2018-06-23 21:19:23 +0200

    Fix entrypoint

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -7,13 +7,13 @@ GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USERNAME}@'%' WITH GRANT 
 EOM
 mysql -u $MYSQL_USERNAME -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOSTNAME < /scripts/drop_db.sql
 echo "Previous dump was dropped"
-stage_url="drukujesz24.pl"
+dump_url="localhost"
 if [ "${LOCAL_ENV}" = "true" ]; then
-  echo "Using local env, seding db dump with 'localhost' instead of '$stage_url'"
-  sed -i "s/$stage_url/localhost/g" /scripts/dump.sql
+  echo "Using local env, seding db dump with 'localhost' instead of '$dump_url'"
+  sed -i "s/$dump_url/localhost/g" /scripts/dump.sql
 else
-  echo "Using local env, seding db dump with 'localhost' instead of '$stage_url'"
-  sed -i "s-$stage_url-195.242.117.177:7000/drukujesz-g" /scripts/dump.sql
+  echo "Using local env, seding db dump with 'localhost' instead of '$dump_url'"
+  sed -i "s-$dump_url-195.242.117.177:7000-g" /scripts/dump.sql
 fi
 mysql -u $MYSQL_USERNAME -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOSTNAME $MYSQL_DATABASE < /scripts/dump.sql
 echo "New dump was applied"
